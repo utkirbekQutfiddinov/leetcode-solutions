14793bc7d0cce29a46ed264d6c9e0ed1a0aaa1bb
Utkirbek Qutfiddinov <39731848+Utkirbek-Qutfiddinov@users.noreply.github.com>
2022-05-29 19:51:20 +0500

    Time: 4 ms (95.54%), Space: 42.5 MB (91.60%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int romanToInt(String s) {
+        if(s.length()==0) return 0;
+        int r=getInt(s.charAt(s.length()-1));
+        for(int i=s.length()-2; i>=0; i--){
+            if(getInt(s.charAt(i))<getInt(s.charAt(i+1))) r-=getInt(s.charAt(i));
+            else r+=getInt(s.charAt(i));
+        }
+        return r;
+    }
+    
+    public int getInt(char c){
+        switch(c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
