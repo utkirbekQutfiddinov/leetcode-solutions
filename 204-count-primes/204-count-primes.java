3e57dfe12be87d9f99d8414b5a1023c1a9f9b612
Utkirbek Qutfiddinov <39731848+Utkirbek-Qutfiddinov@users.noreply.github.com>
2022-05-29 19:24:48 +0500

    Time: 223 ms (23.98%), Space: 68.6 MB (43.17%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,16 +1,15 @@
 class Solution {
     public int countPrimes(int n) {
         if(n<=1) return 0;
+        int res=0;
         boolean[] nums=new boolean[n];
         for(int i=2; i<nums.length; i++){
-            if(nums[i]==false)
+            if(nums[i]==false){
+                res++;
                 for(int j=2*i; j<nums.length; j+=i)
                     nums[j]=true;
+            }
         }
-        
-        int res=0;
-        for(int i=2; i<nums.length; i++)
-            if(nums[i]==false) res++;
         return res;
             
     }
