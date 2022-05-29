ed52f9966ef9a1aba0cc3197eeffb11b9cbfbc52
Utkirbek Qutfiddinov <39731848+Utkirbek-Qutfiddinov@users.noreply.github.com>
2022-05-29 22:27:23 +0500

    Time: 18 ms (23.11%), Space: 45.6 MB (19.34%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -2,7 +2,8 @@ class Solution {
     public boolean isPalindrome(int x) {
         String s=String.valueOf(x);
         for(int i=0; i<=(s.length()/2-1); i++)
-            if(!s.substring(i,i+1).equals(s.substring(s.length()-1-i,s.length()-i)))                                return false;
+            if(s.charAt(i)!=s.charAt(s.length()-1-i))
+            return false;
         return true;
     }
 }
