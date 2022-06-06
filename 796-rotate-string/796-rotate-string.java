118f344dcacc6dc023f8b08598bc23bdafc3eadf
Utkirbek Qutfiddinov <39731848+Utkirbek-Qutfiddinov@users.noreply.github.com>
2022-06-06 20:45:25 +0500

    Time: 1 ms (54.55%), Space: 41.9 MB (56.20%) - LeetHub

--- 796-rotate-string/796-rotate-string.java
+++ 796-rotate-string/796-rotate-string.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        boolean teng=false;
-        int slen=s.length();
-        for(int i=0; i<slen; i++){
-            if(s.equals(goal)) {
-                teng=true; 
-                break;
-            }
-            s=s.substring(1)+s.charAt(0);
-            }
-        return teng;
+        if(s.length()!=goal.length()) return false;
+        return (s+s).contains(goal);
     }
 }
