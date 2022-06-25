ff924ac6d6fcd6b28b47f807560b3d691eb8dad8
Utkirbek Qutfiddinov <39731848+Utkirbek-Qutfiddinov@users.noreply.github.com>
2022-06-25 21:44:35 +0500

    Time: 3 ms (65.27%), Space: 92.9 MB (49.54%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.java
+++ 896-monotonic-array/896-monotonic-array.java
@@ -3,10 +3,11 @@ class Solution {
         boolean res=true;
         if(nums.length==1) return res;
         else if(nums[0]<=nums[nums.length-1])
-        for(int i=0; i<nums.length-1; i++)
-            res=res&&nums[i]<=nums[i+1];
-        else for(int i=0; i<nums.length-1; i++)
-            res=res&&nums[i]>=nums[i+1];
+                for(int i=0; i<nums.length-1; i++)
+                    res=res&&nums[i]<=nums[i+1];
+        else 
+                for(int i=0; i<nums.length-1; i++)
+                    res=res&&nums[i]>=nums[i+1];
         return res;
     }
 }
