7d20eefdc2a4300422916e59bf5c5996604a252f
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2024-02-17 23:55:42 +0500

    Time: 1 ms (17.68%), Space: 41.3 MB (39.87%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words=sentence.split(" +");
+        
+        for(int i=0; i<words.length; i++){
+            if(words[i].startsWith(searchWord)){
+                return i+1;
+            }
+        }
+        
+        return -1;
+    }
+}
