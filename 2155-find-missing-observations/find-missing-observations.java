cca64fada7c9f728b3c60bcd568e395153886e39
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2024-09-05 15:07:52 +0500

    Time: 5 ms (31.80%) | Memory: 59.4 MB (86.89%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.java
+++ 2155-find-missing-observations/find-missing-observations.java
@@ -1,38 +1,34 @@
 class Solution {
     public int[] missingRolls(int[] rolls, int mean, int n) {
-        int sum0=0;
+        int sum0=(rolls.length+n)*mean;
+        int sum1=0;
 
         for(int i: rolls){
-            sum0+=i;
+            sum1+=i;
         }
 
-        int sum=rolls.length*mean+n*mean;
-       int sum1=sum-sum0;
+        int sum2=sum0-sum1;
 
-        System.out.println("mavjud sum: "+sum0);
-        System.out.println("umumiy sum: "+sum);
-        System.out.println("keraklli sum: "+sum1);
-       if(sum1<0 || n>sum1 || sum1>n*6){
-        return new int[0];
-       }
+        if(sum2>6*n || sum2<n){
+            return new int[0];
+        }
 
-       int[] res=new int[n];
+        int[] res=new int[n];
 
-        res[0]=sum1/n;
-        sum1-=res[0];
-       for(int i=1; i<n; i++){
-        res[i]=res[i-1];
-        sum1-=res[0];
-       }
-       System.out.println("massiv :"+Arrays.toString(res));
-       System.out.println("qoldi : "+sum1);
+        int son=sum2/n;
+
+        for(int i=0; i<n; i++){
+            res[i]=son;
+            sum2-=son;
+        }
+
         int ind=0;
-        while(sum1>0){
-            res[ind]++;
-            ind=(ind+1)%n;
-            sum1--;
-        }
 
+        while(sum2>0){
+            res[ind]++;
+            sum2--;
+            ind=(ind+1)%n;
+        }
         return res;
     }
 }
