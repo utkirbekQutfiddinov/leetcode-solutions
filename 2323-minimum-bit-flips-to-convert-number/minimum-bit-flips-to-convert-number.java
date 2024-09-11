61b06188a1ed0f665d276d5a8ad31cd5daae53aa
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2024-09-11 09:34:16 +0500

    Time: 0 ms (100.00%) | Memory: 40.4 MB (64.36%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,44 +1,15 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        StringBuilder s1=getBits(start), s2=getBits(goal);
-
-        if(s1.length()>s2.length()){
-            int dif=s1.length()-s2.length();
-
-
-            for(int i=0; i<dif; i++){
-                s2.insert(0,'0');
-            }
-        }else {
-            int dif=s2.length()-s1.length();
-
-
-            for(int i=0; i<dif; i++){
-                s1.insert(0,'0');
-            }
-        }
-
+        
         int count=0;
 
-        for(int i=0; i<s1.length(); i++){
-            if(s1.charAt(i)!=s2.charAt(i)){
+        while(start>0 ||goal>0){
+            if(start%2!=goal%2){
                 count++;
             }
+            start/=2;
+            goal/=2;
         }
-
         return count;
-
-    }
-
-
-    private StringBuilder getBits(int num){
-        StringBuilder sb=new StringBuilder("");
-
-        while(num!=0){
-            sb.insert(0,num%2);
-            num/=2;
-        }
-
-        return sb;
     }
 }
