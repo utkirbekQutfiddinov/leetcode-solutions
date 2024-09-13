82eab80fb1ebfb90be87497f6e07952dc6d39d56
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2024-09-13 10:06:52 +0500

    Time: 2 ms (100.00%) | Memory: 57.6 MB (55.23%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,14 +1,23 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int[] answer=new int[queries.length];
+       int[] p=new int[arr.length];
 
-        //answer[0]=11^2;
+        p[0]=arr[0];
 
-        for(int i=0; i<queries.length; i++){
-            for(int j=queries[i][0]; j<=queries[i][1]; j++){
-                answer[i]=answer[i]^arr[j];
-            }
+       for(int i=1; i<arr.length; i++){
+        p[i]=p[i-1]^arr[i];
+       }
+
+
+       int[] answer=new int[queries.length];
+
+       for(int i=0; i<queries.length; i++){
+        if(queries[i][0]>0){
+            answer[i]=p[queries[i][1]]^p[queries[i][0]-1];
+        }else {
+            answer[i]=p[queries[i][1]];
         }
+       }
 
         return answer;
     }
