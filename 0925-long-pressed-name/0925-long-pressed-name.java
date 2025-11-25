750b414de289cad501f50ffdd0000549ef64b6b9
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2025-11-25 18:07:44 +0500

    Time: 1 ms (81.42%), Space: 43 MB (9.38%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int n=1, t=1;
+
+        if(name.charAt(0)!=typed.charAt(0)){
+            return false;
+        }
+
+        while(t<typed.length() && n<name.length()){
+            if(typed.charAt(t)==name.charAt(n)){
+                t++;
+                n++;
+            }else if(typed.charAt(t)==typed.charAt(t-1)){
+                t++;
+            }else {
+                return false;
+            }
+        }
+
+        if(n==name.length()){
+            while(t<typed.length()){
+                if(typed.charAt(t)!=typed.charAt(t-1)){
+                    return false;
+                }
+                t++;
+            }
+        }
+
+
+        return t==typed.length() && n==name.length();
+        //return true;
+    }
+}
