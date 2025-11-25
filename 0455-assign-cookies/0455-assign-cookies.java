106986266a68d93d25c0d6cc6f3653676b4d1159
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2025-11-25 11:18:34 +0500

    Time: 13 ms (42.18%), Space: 51.4 MB (19.32%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int res=0;
+        int i=0, j=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                res++;
+                i++;
+            }
+            j++;
+        }
+        return res;
+    }
+}
