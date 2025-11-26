0108775a8dd6feacb08bdb99251b4f1b6899314f
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2025-11-26 14:37:38 +0500

    Time: 1 ms (98.39%), Space: 47 MB (11.14%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -12,7 +12,6 @@ class Solution {
         int l=0,r=arr.length-1;
 
         while(i>=0){
-          //  System.out.print("i="+i+", r="+r+", [");
             if(arr[i]==0){
                 if(resLen>arr.length){
                     arr[r]=0;
@@ -29,15 +28,6 @@ class Solution {
                 r--;
             }
             i--;
-            
-            //for(int j=0; j<arr.length; j++){
-             //   if(j==arr.length-1)
-            // System.out.print(arr[j]+"]"); 
-             //else
-            // System.out.print(arr[j]+", ");   
-          //  }
-            
-           // System.out.println("i="+i+", r="+r+";");
         }
 
 
