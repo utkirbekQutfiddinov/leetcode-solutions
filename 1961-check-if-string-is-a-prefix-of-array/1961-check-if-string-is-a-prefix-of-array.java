568585f68f7818575547657d8a08fc215cf1b0a9
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2025-11-26 18:47:39 +0500

    Time: 1 ms (81.99%), Space: 44.3 MB (5.72%) - LeetHub

---
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        StringBuilder sb=new StringBuilder("");
+
+        for(String word: words){
+            sb.append(word);
+
+            if(sb.toString().equals(s)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
