699cd992610abdb9c422e7aee2925226d65313d1
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2025-11-27 13:48:33 +0500

    Time: 1 ms (97.9%), Space: 45.1 MB (16.47%) - LeetHub

---
+++ 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        for(int i=0; i<nums.length-indexDifference; i++){
+            for(int j=i+indexDifference; j<nums.length; j++){
+                if(Math.abs(nums[i]-nums[j])>=valueDifference){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
