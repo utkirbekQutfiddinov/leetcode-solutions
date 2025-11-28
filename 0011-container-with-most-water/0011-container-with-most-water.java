d1dbea8539603eeecd479d0abe696b9f84bfec1e
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2025-11-28 12:00:41 +0500

    Time: 4 ms (92.06%), Space: 77.2 MB (16.03%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0, r=height.length-1;
+        int maxArea=0, currArea=0, lastArea=0;
+        while(l<r){
+            currArea=Math.min(height[l],height[r])*(r-l);
+            if(currArea>maxArea){
+                maxArea=currArea;
+            }
+            lastArea=currArea;
+             if(height[l]>height[r]){
+                    r--;
+                }else {
+                    l++;
+                }
+        }
+
+    return maxArea;
+
+    }
+}
