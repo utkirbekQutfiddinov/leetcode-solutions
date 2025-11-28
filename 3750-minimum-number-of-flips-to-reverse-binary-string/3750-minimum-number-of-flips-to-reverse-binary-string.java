66a3a0cb139ece3683549e8814a7d5c8a2c9bb52
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2025-11-28 11:37:57 +0500

    Time: 2 ms (66.32%), Space: 42.7 MB (69.98%) - LeetHub

--- 3750-minimum-number-of-flips-to-reverse-binary-string/3750-minimum-number-of-flips-to-reverse-binary-string.java
+++ 3750-minimum-number-of-flips-to-reverse-binary-string/3750-minimum-number-of-flips-to-reverse-binary-string.java
@@ -1,19 +1,11 @@
 class Solution {
     public int minimumFlips(int n) {
-        StringBuilder sb=new StringBuilder("");
-        while(n>0){
-            sb.insert(0,n%2);
-            n/=2;
-        }
-
-        char[] c1=sb.toString().toCharArray();
-        char[] c2=sb.reverse().toString().toCharArray();
-
         int res=0;
+        String s=Integer.toBinaryString(n);
 
-        for(int i=0; i<c1.length; i++){
-            if(c1[i]!=c2[i]){
-                res++;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+               res++; 
             }
         }
 
