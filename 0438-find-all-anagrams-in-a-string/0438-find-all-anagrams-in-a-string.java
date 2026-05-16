e0cc4522ae356b96bba9e7b6b2a585c3b067949b
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2026-05-16 14:11:14 +0500

    Time: 12 ms (54.01%), Space: 46.9 MB (45.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,24 +1,21 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int left=0, right=0;
-        int[] bp=new int[26];
-        int[] bs=new int[26];
-        for(char c: p.toCharArray()){
-            bp[c-97]++;
+        List<Integer> res = new ArrayList<>();
+        if (p.length() > s.length()) return res;
+
+        int[] bp = new int[26];
+        int[] bs = new int[26];
+
+        for (char c : p.toCharArray()) bp[c - 'a']++;
+        for (int i = 0; i < p.length(); i++) bs[s.charAt(i) - 'a']++;
+
+        for (int i = p.length(); i < s.length(); i++) {
+            if (Arrays.equals(bp, bs)) res.add(i - p.length());
+            bs[s.charAt(i - p.length()) - 'a']--;
+            bs[s.charAt(i) - 'a']++;
         }
 
-        List<Integer> res=new ArrayList<>();
-        for(int i=0; i<p.length()&&i<s.length(); i++){
-            bs[s.charAt(i)-97]++;
-        }
-
-        for(int i=p.length(); i<s.length(); i++){
-            if(Arrays.equals(bp,bs)) res.add(i-p.length());
-            bs[s.charAt(i-p.length())-97]--;
-            bs[s.charAt(i)-97]++;
-        }
-
-            if(Arrays.equals(bp,bs)) res.add(s.length()-p.length());
-            return res;
+        if (Arrays.equals(bp, bs)) res.add(s.length() - p.length());
+        return res;
     }
 }
