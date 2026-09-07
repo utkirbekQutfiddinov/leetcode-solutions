bb610bb44be555eaacec741c9a2c9c9c8214ec84
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2026-09-07 09:27:30 +0500

    Time: 1 ms (100%), Space: 41.8 MB (100%) - LeetHub

--- 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.java
+++ 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.java
@@ -1,5 +1,15 @@
 class Solution {
     public long removeZeros(long n) {
-        return Long.parseLong(String.valueOf(n).replaceAll("0",""));
+        long res=0l;
+        long coeff=1;
+        while(n>0){
+            long rem=n%10;
+            if(rem>0){
+                res+=rem*coeff;
+                coeff*=10;
+            }
+            n/=10;
+        }
+        return res;
     }
 }
