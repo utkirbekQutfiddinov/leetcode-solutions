119eadf4656973802649500f43c76b1b52501f1d
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2026-09-14 15:10:12 +0500

    Time: 0 ms (100%), Space: 42.6 MB (52.92%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isRectangleOverlap(int[] r1, int[] r2) {
-        return r1[0] < r2[2] && r2[0] < r1[2] &&
-               r1[1] < r2[3] && r2[1] < r1[3];
+        boolean a=!(r1[2]<=r2[0] || r2[2]<=r1[0])&& !(r1[3]<=r2[1] || r2[3]<=r1[1]);
+        return a;
     }
 }
