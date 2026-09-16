74c785273ba426bdd21af97343b3739d41642c16
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2026-09-16 17:00:47 +0500

    Time: 26 ms (5.79%), Space: 48.5 MB (5.14%) - LeetHub

---
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] minDistinctFreqPair(int[] nums) {
+        Map<Integer, Integer> freqs=new HashMap<>();
+        for(int num: nums){
+            freqs.merge(num, 1, Integer::sum);
+        }
+
+        System.out.println("freqs: " + freqs);
+
+        Map<Integer, List<Integer>> groups=new HashMap<>();
+        for(int num: freqs.keySet()){
+            int freq=freqs.get(num);
+            List<Integer> group=groups.getOrDefault(freq,new ArrayList<>());
+            group.add(num);
+            groups.put(freq, group);
+        }
+        
+        System.out.println("groups: " + groups);
+        if(groups.size()<2) return new int[]{-1,-1};
+
+        int min1=Integer.MAX_VALUE, min2=Integer.MAX_VALUE;
+
+        for(int freq: groups.keySet()){
+            List<Integer> group=groups.get(freq);
+
+            int num=group.get(0);
+
+            for(int el: group){
+                if(el<num){
+                    num=el;
+                }
+            }
+
+            if(num<min1){
+                min2=min1;
+                min1=num;
+            }else if(num<min2){
+                min2=num;
+            }
+        }
+        System.out.println("min1: " + min1+", min2: "+min2);
+        if(min1<min2){
+            return new int[]{min1,min2};
+        }else {
+            return new int[]{min2,min1};
+        }
+    }
+}
