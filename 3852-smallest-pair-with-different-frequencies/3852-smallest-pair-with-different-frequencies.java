300a500ab3eebbe7940393b2024c2011376be5b8
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2026-09-16 17:02:19 +0500

    Time: 24 ms (5.79%), Space: 48.6 MB (5.14%) - LeetHub

--- 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] minDistinctFreqPair(int[] nums) {
         Map<Integer, Integer> freqs=new HashMap<>();
+        //har bir sonning chastotasini topvolamiz
         for(int num: nums){
             freqs.merge(num, 1, Integer::sum);
         }
 
         System.out.println("freqs: " + freqs);
 
+        //chastotasi bir xil bo'lgan sonlarni guruhlaymiz
         Map<Integer, List<Integer>> groups=new HashMap<>();
         for(int num: freqs.keySet()){
             int freq=freqs.get(num);
@@ -20,17 +22,20 @@ class Solution {
 
         int min1=Integer.MAX_VALUE, min2=Integer.MAX_VALUE;
 
+        //har bir chastota guruhi bo'yicha eng kichik sonlardan ikkitasi topamiz
         for(int freq: groups.keySet()){
             List<Integer> group=groups.get(freq);
 
             int num=group.get(0);
 
+            //shu chastota guruhi ichidagi eng kichik sonni topamiz
             for(int el: group){
                 if(el<num){
                     num=el;
                 }
             }
 
+            //topilgan eng kichik son min1 yki min2 bilan solishtiriladi
             if(num<min1){
                 min2=min1;
                 min1=num;
