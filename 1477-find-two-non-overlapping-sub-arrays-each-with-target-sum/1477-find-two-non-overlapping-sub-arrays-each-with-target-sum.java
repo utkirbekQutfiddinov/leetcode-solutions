468e7b3b5b357ad82d12993b1c35f1f76a84c016
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2026-09-18 10:07:08 +0500

    Time: 5 ms (100%), Space: 91.1 MB (50%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,80 +1,31 @@
 class Solution {
     public int minSumOfLengths(int[] arr, int target) {
-        List<int[]> list=new ArrayList<>();
-        int l=0, r=-1;
-        int sum=0;
-        int res=Integer.MAX_VALUE;
+        int n = arr.length;
+        int[] minLen = new int[n];
+        int l = 0, sum = 0;
+        int ans = Integer.MAX_VALUE;
+        int best = Integer.MAX_VALUE;
         
-        while(r<arr.length-1){
-            if(sum<target){
-                r++;
-                sum+=arr[r];
-            }else if(sum==target){
-                list.add(new int[]{l,r});
-                sum-=arr[l];
-                l++;
-            }else {
-                sum-=arr[l];
-                l++;
+        for (int r = 0; r < n; r++) {
+            sum += arr[r];
+            
+            while (sum > target) {
+                sum -= arr[l++];
             }
-        }
-
-        while(sum>target){
-            sum-=arr[l];
-            l++;
-        }
-
-        if(sum==target){
-            list.add(new int[]{l,r});
-        }
-
-        for(int[] pair: list){
-            System.out.println(Arrays.toString(pair));
-        }
-
-        if(list.size()<2) return -1;
-
-
-        l=0;
-        r=1;
-        int[] left=list.get(l), right=list.get(r);
-
-        while(right[0]<=left[1]){
-            r++;
-            if(r==list.size()) break;
-            right=list.get(r);
-        }
-        
-        System.out.println("Starting pairs:"+Arrays.toString(left)+":"+Arrays.toString(right));
-        System.out.println();
-
-        if(right[0]<=left[1]) return -1;
-        
-        res=Math.min(res, right[1]-right[0]+1+left[1]-left[0]+1);
-
-        int minInd=l;
-        int[] min=left;
-
-        while(r<list.size()){
-            right=list.get(r);
-            while(l<r && left[1]<right[0]){
-                l++;
-                left=list.get(l);
-
-                if(left[1]>=right[0]){
-                    l--;
-                    break;
+            
+            if (sum == target) {
+                int currLen = r - l + 1;
+                
+                if (l > 0 && minLen[l - 1] != Integer.MAX_VALUE) {
+                    ans = Math.min(ans, currLen + minLen[l - 1]);
                 }
-
-                if(left[1]-left[0]<min[1]-min[0]){
-                    minInd=l;
-                    min=left;
-                }
+                
+                best = Math.min(best, currLen);
             }
-            res=Math.min(res, right[1]-right[0]+1+min[1]-min[0]+1);
-            r++;
+            
+            minLen[r] = best;
         }
-
-       return res==Integer.MAX_VALUE?-1:res;
+        
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
