bcd0408977f738775c4f920932d6aa20dd89de2f
Utkirbek Qutfiddinov <39731848+utkirbekQutfiddinov@users.noreply.github.com>
2026-09-22 12:47:08 +0500

    Time: 1 ms (99.49%), Space: 46.8 MB (87.37%) - LeetHub

---
+++ 3402-minimum-operations-to-make-columns-strictly-increasing/3402-minimum-operations-to-make-columns-strictly-increasing.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumOperations(int[][] grid) {
+        int res=0;
+        for(int i=1; i<grid.length; i++){
+            for(int j=0; j<grid[i].length; j++){
+                if(grid[i][j]<=grid[i-1][j]){
+                    int diff=grid[i-1][j]-grid[i][j]+1;
+                    res+=diff;
+                    grid[i][j]+=diff;
+                }
+            }
+        }
+        return res;
+    }
+}
